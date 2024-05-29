1f2e24f478d52c4a167f6873c331bf718e46fff6
meowmers321 <mrmeowmers321@gmail.com>
2024-05-29 15:12:04 -0700

    character controller

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpHeight = 10f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    private void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        float moveDirection = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+        Debug.Log(isGrounded);
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        }
+    }
+}
